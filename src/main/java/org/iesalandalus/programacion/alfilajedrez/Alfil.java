c44032d91a2513c4bad3830ec4104cfcdd5bd176
selic <selic@192.168.1.108>
2020-11-20 17:20:18 +0100

    Creó los metodos get y set para la clase alfil.

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -3,4 +3,22 @@ package org.iesalandalus.programacion.alfilajedrez;
 public class Alfil {
 	public  Color color;
 	public Posicion posicion;
+	public Color getColor() {
+		return color;
+	}
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new IllegalArgumentException ("ERROR: El color no puede ser nulo.");
+		}
+		this.color = color;
+	}
+	public Posicion getPosicion() {
+		return posicion;
+	}
+	private void setPosicion(Posicion posicion) {
+		if (posicion == null) {
+			throw new IllegalArgumentException("La posición no puede ser nula.");
+		}
+		this.posicion = posicion;
+	}
 }
