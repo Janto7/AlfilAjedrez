082cf82b3cb0a5aee9436817790017b8cf35da88
selic <selic@192.168.1.108>
2020-11-22 03:48:46 +0100

    creó los metodos get y set para los atributos con los mensajes adecuados

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/Alfil.java
@@ -1,6 +1,30 @@
 package org.iesalandalus.programacion.alfilajedrez;
 
 public class Alfil {
-	private  Color color;
+	private Color color;
 	private Posicion posicion;
+
+	public Color getColor() {
+		return color;
+	}
+
+	public void setColor(Color color) {
+		if (color == null) {
+			throw new IllegalArgumentException("ERROR:El color no puede ser nulo.");
+		}
+		this.color = color;
+
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+		if (posicion==null) {
+			throw new IllegalArgumentException("ERROR:La posición no puede ser nula.");
+		}
+		this.posicion = posicion;
+	}
+
 }
