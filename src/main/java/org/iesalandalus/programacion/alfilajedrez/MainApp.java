8a28895dc9e872c9cb3fdab35a260ea3d47ff0b0
Janto <selicoslive@gmail.com>
2020-12-11 19:11:08 +0100

    Creo el metodo mostrar menu

--- src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/alfilajedrez/MainApp.java
@@ -11,11 +11,19 @@ public class MainApp {
 	public static void main(String[] args) {
 
 		
-
+	}
 	private static void mostrarAlfil() {
 		if(alfil != null)
 			System.out.println(alfil);
 		else
 			System.out.println("Alfil sin datos");
+	}
+	private static void mostrarMenu() {
+		System.out.println("MENU PRINCIPAL");
+		System.out.println("1.- Crear alfil por defecto");
+		System.out.println("2.- Crear un alfil de un color");
+		System.out.println("3.- Crar un alfil en un color y columna inicial");
+		System.out.println("4.- Mover el alfil");
+		System.out.println("5.- Salir");
 	}
 	}
